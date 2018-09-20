2f057976a16c5b5b07f48d308a060d606e146a7f
Allen Qu <aqu1@bu.edu>
2018-09-19 21:53:51 -0400

    added graphing changes (graphing for both temp and humid) to project

--- app/src/main/java/com/example/edejesus1097/miniproj/Graphing.java
+++ app/src/main/java/com/example/edejesus1097/miniproj/Graphing.java
@@ -14,10 +14,13 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Graphing extends AppCompatActivity {
-    String storeVals[]; // stores time and temperature value from single row in CSV
-    Double time;        // time value pulled from a single row in the CSV
-    Double temp;        // temperature value pulled from a single row in the CSV
-    GraphView graph;    // graph object for graphing
+    String storeVals[];       // stores time and temperature value from single row in CSV
+    String storeTitle[];      // store first line of CSV to distinguish between temperature and humidity
+    String storeX;            // store string in first row and column of CSV (always "time")
+    String timeOrHumid;       // store string in first row and second column of CSV ("time" or "humidity")
+    Double time;              // time value pulled from a single row in the CSV
+    Double temp_humid;        // temperature or humidity value pulled from a single row in the CSV
+    GraphView graph;          // graph object for graphing
 
     private LineGraphSeries<DataPoint> series1; // stores all of the data from CSV for graphing
 
@@ -30,12 +33,17 @@ public class Graphing extends AppCompatActivity {
 
         // initialize reader to read CSV
         BufferedReader reader = null;
+        String title;
         try {
-            //Uri uri;
-            //ContentResolver cr = getContentResolver();
-            //InputStream is = cr.openInputStream(uri);
-            reader = new BufferedReader(new InputStreamReader(getAssets().open("temptime_csv"))); // read from CSV file
-            reader.readLine();  //skip first line of file
+            Uri uri;
+            ContentResolver cr = getContentResolver();
+            InputStream is = cr.openInputStream(uri);
+            reader = new BufferedReader(new InputStreamReader(is)); // read from CSV file
+            title = reader.readLine();  //skip first line of file
+            storeTitle = title.split(",");
+            storeX = storeTitle[0];
+            timeOrHumid = storeTitle[1];
+
             String comma; // used to separate time and temp by comma in file
 
             ArrayList<DataPoint> dataPoints = new ArrayList<>(); // create an ArrayList to store all time and temp values
@@ -44,15 +52,15 @@ public class Graphing extends AppCompatActivity {
             while ((comma = reader.readLine()) != null) {
                 storeVals = comma.split(",");
                 time = Double.parseDouble(storeVals[0]);
-                temp = Double.parseDouble(storeVals[1]);
+                temp_humid = Double.parseDouble(storeVals[1]);
 
-                DataPoint dp = new DataPoint(time, temp);
+                DataPoint dp = new DataPoint(time, temp_humid);
                 dataPoints.add(dp);
             }
 
             // transfer data from ArrayList to an Array
             DataPoint[] allData = new DataPoint[dataPoints.size()];
-            for(int i = 0; i < dataPoints.size(); i++){
+            for (int i = 0; i < dataPoints.size(); i++) {
                 allData[i] = dataPoints.get(i);
             }
 
@@ -62,10 +70,9 @@ public class Graphing extends AppCompatActivity {
 
         } catch (IOException e) {
             e.printStackTrace();
-            finish();
         }
 
-        // Customize numbers for X and Y axes
+        // Customize numbers for X and Y axes for graph
         graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
             @Override
             public String formatLabel(double value, boolean isValueX) {
@@ -79,37 +86,86 @@ public class Graphing extends AppCompatActivity {
             }
         });
 
-        // set title of graph
-        graph.setTitle("Temperature Over The Next Day");
+        if (timeOrHumid == "temperature") {
+            // set title of graph
+            graph.setTitle("Temperature Over The Next Day");
 
-        // set the X-axis bounds manually
-        graph.getViewport().setXAxisBoundsManual(true);
+            // set the X-axis of bounds manually
+            graph.getViewport().setXAxisBoundsManual(true);
 
-        // set min value on X-axis to 0
-        graph.getViewport().setMinX(0);
+            // set min value on X-axis of to 0
+            graph.getViewport().setMinX(0);
 
-        // set max value on X-axis to 24
-        graph.getViewport().setMaxX(24);
+            // set max value on X-axis to 24
+            graph.getViewport().setMaxX(24);
 
-        // set X-axis label
-        graph.getGridLabelRenderer().setHorizontalAxisTitle("Time");
+            // set X-axis label
+            graph.getGridLabelRenderer().setHorizontalAxisTitle("Time");
 
-        // set Y-axis label
-        graph.getGridLabelRenderer().setVerticalAxisTitle("Temperature (°F)");
+            // set Y-axis label
+            graph.getGridLabelRenderer().setVerticalAxisTitle("Temperature (°F)");
 
-        // enables horizontal zooming and scrolling
-        graph.getViewport().setScalable(true);
+            // enables horizontal zooming and scrolling
+            graph.getViewport().setScalable(true);
 
-        // enables horizontal scrolling
-        graph.getViewport().setScrollable(true);
+            // enables horizontal scrolling
+            graph.getViewport().setScrollable(true);
 
-        // enables horizontal and vertical zooming and scrolling
-        graph.getViewport().setScalableY(true);
+            // enables horizontal and vertical zooming and scrolling
+            graph.getViewport().setScalableY(true);
 
-        // enables vertical scrolling
-        graph.getViewport().setScrollableY(true);
+            // enables vertical scrolling
+            graph.getViewport().setScrollableY(true);
+        }
+
+        if (timeOrHumid == "humidity") {
+            // set title of graph
+            graph.setTitle("Humidity Over The Next Day");
+
+            // set the X-axis bounds manually
+            graph.getViewport().setXAxisBoundsManual(true);
+
+            // set the Y-axis bounds manually
+            graph.getViewport().setYAxisBoundsManual(true);
+
+            // set min value on X-axis to 0
+            graph.getViewport().setMinX(0);
+
+            // set max value on X-axis to 24
+            graph.getViewport().setMaxX(24);
+
+            // set min value on Y-axis to 0
+            graph.getViewport().setMinY(0);
+
+            // set max value on Y-axis to 100
+            graph.getViewport().setMaxY(100);
+
+            // set X-axis label
+            graph.getGridLabelRenderer().setHorizontalAxisTitle("Time");
+
+            // set Y-axis label
+            graph.getGridLabelRenderer().setVerticalAxisTitle("Humidity (%)");
+
+            // enables horizontal zooming and scrolling
+            graph.getViewport().setScalable(true);
+
+            // enables horizontal scrolling
+            graph.getViewport().setScrollable(true);
+
+            // enables horizontal and vertical zooming and scrolling
+            graph.getViewport().setScalableY(true);
+
+            // enables vertical scrolling
+            graph.getViewport().setScrollableY(true);
+        }
     }
 }
+
+
+
+
+
+
 
 
 
